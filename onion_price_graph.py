5761c66f48551de0359429fbf5cd532c4333465e
Anupriyam Shukla <60869367+Anupriyam-FOSD@users.noreply.github.com>
2022-10-01 19:36:58 +0530

    Created onion_price_graph.py

---
+++ onion_price_graph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+week = [1, 2, 3, 4]
+Prices = [40, 80, 100, 50]
+#Plotting line graph
+plt.plot(week, Prices)
+#Set the x axis and y axis labels
+plt.xlabel('Week')
+plt.ylabel('Onion Prices (Rs.)')
+plt.show()
